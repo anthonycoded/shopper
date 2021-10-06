8dd56eb0db05463ba2aaadf5cda4edc39ac57b98
shawn barbel <barbels33@gmail.com>
2021-10-06 16:41:14 -0400

    created explore scren=en blank

---
+++ src/screens/ExploreScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ExploreScreen = () => {
+  return (
+    <View>
+      <Text>Explore Screen</Text>
+      <Text>Search Bar</Text>
+      <Text>Trending</Text>
+    </View>
+  );
+};
+
+export default ExploreScreen;
