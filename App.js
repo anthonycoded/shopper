cf2205c2473e34d7cc12abf2bc1ed6e889544a31
shawn barbel <barbels33@gmail.com>
2021-10-06 16:39:28 -0400

    created main navigator and provided store

--- App.js
+++ App.js
@@ -1,23 +1,41 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import SearchScreen from "./src/screens/SearchScreen";
+import { Provider } from "react-redux";
+import store from "./src/store/Store";
+import DrawerNavigator from "./src/navigation/DrawerNavigator";
+import { config } from "./src/config/Config";
+import { theme } from "./src/config/Theme";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer screenOptions={{}}>
-      <Stack.Navigator
-        initialRouteName=""
-        screenOptions={{
-          headerTitle: "React-Native-Template",
-        }}
-      >
-        <Stack.Screen name="Search" component={SearchScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName=""
+          screenOptions={{
+            title: "Threadly",
+            headerTitleAlign: "center",
+            headerStyle: {
+              backgroundColor: theme.colors.primary,
+              textAlign: "center",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        >
+          <Stack.Screen
+            name="Drawer"
+            component={DrawerNavigator}
+            options={{}}
+          ></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
