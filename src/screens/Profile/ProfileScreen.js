2c6e339624a9ceabc9252a59e9a29508a1b34e01
shawn barbel <barbels33@gmail.com>
2021-10-06 16:41:48 -0400

    profle screen

---
+++ src/screens/Profile/ProfileScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ProfileScreen = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 22 }}>Profile Screen</Text>
+      <Text>MEssages</Text>
+      <Text>Orders</Text>
+      <Text>Preferences</Text>
+      <Text>Settings</Text>
+    </View>
+  );
+};
+
+export default ProfileScreen;
