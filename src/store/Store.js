1504f4abefb245a4f1adf408c71c99af0c6522a2
shawn barbel <barbels33@gmail.com>
2021-10-06 16:41:59 -0400

    created redux store

---
+++ src/store/Store.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers } from "redux";
+
+import { authReducer } from "./reducers/AuthReducer";
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const store = createStore(rootReducer);
+
+export default store;
