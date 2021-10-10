23310a1df57c83cdf1471317b2b20a32caf482fc
shawn barbel <barbels33@gmail.com>
2021-10-10 16:20:18 -0400

    added mens and womans section

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,15 +2,19 @@ import React from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
 import ForYouSection from "../components/Home/ForYouSection";
 import Trending from "../components/Home/Trending";
+import Womans from "../components/Home/Womans";
 import AuthSection from "../components/Home/AuthSection";
 
 import { config } from "../config/Config";
 import { theme } from "../config/Theme";
+import Mens from "../components/Home/Mens";
 
 const HomeScreen = () => {
   return (
     <ScrollView style={styles.container}>
       <AuthSection config={config}></AuthSection>
+      <Womans></Womans>
+      <Mens></Mens>
       <ForYouSection></ForYouSection>
       <Trending></Trending>
     </ScrollView>
