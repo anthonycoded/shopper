09404b2ccfaf2311b342f4114f67671d231964d7
shawn barbel <barbels33@gmail.com>
2021-10-10 16:19:58 -0400

    added bag to header rught

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -1,17 +1,44 @@
 import React from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import BottomTabNavigator from "./BottomTabNavigator";
 import DrawerContent from "./DrawerContent";
+import { Feather } from "@expo/vector-icons";
+import { Fontisto } from "@expo/vector-icons";
+import { theme } from "../config/Theme";
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator drawerContent={(props) => <DrawerContent {...props} />}>
+    <Drawer.Navigator
+      screenOptions={{
+        title: "Threadly",
+        headerTitleAlign: "center",
+        headerStyle: {
+          backgroundColor: theme.colors.primary,
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+        headerRight: () => (
+          <TouchableOpacity
+            style={{ marginRight: 10 }}
+            onPress={() => console.log("shopping cart")}
+          >
+            <Fontisto name="shopping-bag-1" size={24} color="black" />
+          </TouchableOpacity>
+        ),
+      }}
+      drawerContent={(props) => <DrawerContent {...props} />}
+    >
       <Drawer.Screen
         name="Shopper"
         component={BottomTabNavigator}
-        options={{ headerShown: false }}
+        options={({ navigation }) => ({
+          headerShown: true,
+        })}
       ></Drawer.Screen>
     </Drawer.Navigator>
   );
